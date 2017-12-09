fa5146b4aecd3ff7ccd07c48d42e4036c8ad9620
namneet singh <namneet@revinfotech.com>
2017-12-09 17:33:54 +0530

    upload image on azure

---
+++ ResizeImage/ResizeImage.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using System.Web;
+using System.Net;
+
+namespace ResizeImage
+{
+    public class ResizeImage
+    {
+        public static string ResizeOrignalImageToThumbImage(string ImageName,string ImageExtension,string OrignalImagePath, Image OrignalImage,string SaveOrignalImagePath,string SaveThumbImagePath,int ResizeWidth,int ResizeHeight)
+        {
+            string result = "";
+            string imageName = Path.GetFileName(ImageName);            
+            try
+            {
+                string originalImagePath = Path.Combine(HttpContext.Current.Server.MapPath(SaveOrignalImagePath));
+                bool originalImagefolderExists = Directory.Exists(originalImagePath);
+                if (!originalImagefolderExists)
+                    Directory.CreateDirectory(originalImagePath);
+                string originalImagefile = Path.Combine(originalImagePath, ImageName + "." + ImageExtension);
+                if(!string.IsNullOrEmpty(OrignalImagePath))
+                {
+                    Image image = DownloadImageFromUrl(OrignalImagePath);
+                    if (image != null)
+                    {
+                        Image thumbImage = image.GetThumbnailImage(ResizeWidth, ResizeHeight, () => false, IntPtr.Zero);
+                        string thumbImagePath = HttpContext.Current.Server.MapPath(SaveThumbImagePath);
+                        bool thumbImagefolderExists = Directory.Exists(thumbImagePath);
+                        if (!thumbImagefolderExists)
+                            Directory.CreateDirectory(thumbImagePath);
+                        string thumbImagefile = Path.Combine(thumbImagePath, ImageName + "_thumb" + "." + ImageExtension);
+
+                        thumbImage.Save(thumbImagefile);
+                        image.Save(originalImagefile);
+                        result = "Image Resize Successfully";
+                    }
+                }
+                else
+                {
+                    Image image = OrignalImage;
+                    if (image != null)
+                    {
+                        Image thumbImage = image.GetThumbnailImage(ResizeWidth, ResizeHeight, () => false, IntPtr.Zero);
+                        string thumbImagePath = HttpContext.Current.Server.MapPath(SaveThumbImagePath);
+                        bool thumbImagefolderExists = Directory.Exists(thumbImagePath);
+                        if (!thumbImagefolderExists)
+                            Directory.CreateDirectory(thumbImagePath);
+                        string thumbImagefile = Path.Combine(thumbImagePath, ImageName + "_thumb" + "." + ImageExtension);
+
+                        thumbImage.Save(thumbImagefile);
+                        image.Save(originalImagefile);
+                        result = "Image Resize Successfully";
+                    }
+                }                
+                return result;
+            }
+            catch(Exception ex)
+            {
+                result = ex.Message;
+                return result;
+            }            
+        }
+
+        public static Image DownloadImageFromUrl(string imageUrl)
+        {
+            Image image = null;
+            try
+            {
+                HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(imageUrl);
+                webRequest.AllowWriteStreamBuffering = true;
+                webRequest.Timeout = 30000;
+                WebResponse webResponse = webRequest.GetResponse();
+                Stream stream = webResponse.GetResponseStream();
+                image = Image.FromStream(stream);
+                webResponse.Close();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            return image;
+        }
+    }
+}
---
+++ ResizeImage/UploadImageCloud.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace ResizeImage
+{
+    public class UploadImageCloud
+    {
+        public static string UploadImageOnAzureBlob(string StorageConnectionString,string containerName,string ImageName,string ImageExtension,string ImagePathFromImageSave)
+        {
+            string result = "";
+            try
+            {
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(StorageConnectionString);
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+                CloudBlobContainer ThumbImage_container = blobClient.GetContainerReference(containerName);
+                if (container.CreateIfNotExists())
+                {
+                    // configure container for public access
+                    var permissions = container.GetPermissions();
+                    permissions.PublicAccess = BlobContainerPublicAccessType.Container;
+                    container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+                }
+
+                CloudBlockBlob cloudBlockBlobThumbImage = container.GetBlockBlobReference(ImageName + "." + ImageExtension); //file name
+                cloudBlockBlobThumbImage.Properties.ContentType = "image/" + ImageExtension;
+                using (var fileStream = File.OpenRead(ImagePathFromImageSave + "/" + ImageName + "." + ImageExtension))
+                {
+                    cloudBlockBlobThumbImage.UploadFromStream(fileStream);
+                }
+                Console.WriteLine("Blob_thumImage Url : {0}", cloudBlockBlobThumbImage.Uri);
+                result = cloudBlockBlobThumbImage.Uri.ToString();
+                result = "Image Upload Successfully";
+                return result;
+            }
+            catch(Exception ex)
+            {
+                result = ex.Message;
+                return result;
+            }            
+        }
+
+        public Stream GetStream(Image img, ImageFormat format)
+        {
+            var ms = new MemoryStream();
+            img.Save(ms, format);
+            return ms;
+        }
+    }
+}
